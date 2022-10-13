8a794f9ad0bb9368f0db8534607fe58494b1b775
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-10-13 09:55:37 -0600

    reading to file

--- stockgen/src/main/java/stock/App.java
+++ stockgen/src/main/java/stock/App.java
@@ -7,6 +7,15 @@ import org.json.simple.parser.*;
 
 public class App {
     public static void main (String[] args){
-        
+        JSONParser parser = new JSONParser();
+
+        try{
+            Object obj = parser.parse(new FileReader("stock_transations-3.by.account.holder.json"));
+            JSONObject jsonObject = (JSONObject)obj;
+            String name = (String)jsonObject.get("first_name");
+            System.out.println("name:" + name);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
