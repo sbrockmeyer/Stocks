647894b2b81f9c13ee2c1e1c0f6190077777f7dd
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-10-13 10:01:30 -0600

    reading some more

--- stockgen/src/main/java/stock/App.java
+++ stockgen/src/main/java/stock/App.java
@@ -12,8 +12,9 @@ public class App {
         try{
             Object obj = parser.parse(new FileReader("stock_transations-3.by.account.holder.json"));
             JSONObject jsonObject = (JSONObject)obj;
-            String name = (String)jsonObject.get("first_name");
-            System.out.println("name:" + name);
+            String Fname = (String)jsonObject.get("first_name");
+            String Lname = (String)jsonObject.get("last_name");
+            System.out.println("Name: " + Fname + Lname);
         }catch(Exception e){
             e.printStackTrace();
         }
