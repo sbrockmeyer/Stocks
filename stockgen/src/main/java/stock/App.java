fe7ba73689ec91cb903985adf52fcb9945faec8c
sbrockmeyer <79857788+sbrockmeyer@users.noreply.github.com>
2022-10-13 15:19:41 -0600

    reading and printing

--- stockgen/src/main/java/stock/App.java
+++ stockgen/src/main/java/stock/App.java
@@ -1,7 +1,7 @@
 package stock;
 
 import java.io.*;
-import java.util.*;
+// import java.util.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
@@ -10,11 +10,17 @@ public class App {
         JSONParser parser = new JSONParser();
 
         try{
-            Object obj = parser.parse(new FileReader("stock_transations-3.by.account.holder.json"));
-            JSONObject jsonObject = (JSONObject)obj;
-            String Fname = (String)jsonObject.get("first_name");
-            String Lname = (String)jsonObject.get("last_name");
-            System.out.println("Name: " + Fname + Lname);
+            Object obj = parser.parse(new FileReader("stockgen/stock_transations-3.by.account.holder.json"));
+            // JSONObject jsonObject = (JSONObject)obj;
+            JSONArray array = (JSONArray)obj;
+            System.out.println(array);
+            // String Fname = (String)jsonObject.get("first_name");
+            // String Lname = (String)jsonObject.get("last_name");
+            // long accountNum = (long)jsonObject.get("account_number");
+            // String social = (String)jsonObject.get("ssn");
+
+
+            // System.out.println("Name: " + Fname + Lname);
         }catch(Exception e){
             e.printStackTrace();
         }
--- stockgen/src/test/java/stock/AppTest.java
+++ stockgen/src/test/java/stock/AppTest.java
@@ -2,7 +2,7 @@ package stock;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
@@ -13,6 +13,6 @@ class AppTest {
      */
     @Test
     void testApp() {
-        
+        System.out.println("hello");
     }
 }
