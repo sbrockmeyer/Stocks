3e5a2ccd20ece2efd2d1c60d70128804727a8e16
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-10-17 09:54:33 -0600

    e

--- stockgen/src/main/java/stock/App.java
+++ stockgen/src/main/java/stock/App.java
@@ -10,31 +10,35 @@ public class App {
         JSONParser parser = new JSONParser();
 
         try{
-            // Object obj = parser.parse(new FileReader("stockgen/stock_transations-3.by.account.holder.json"));
-            Object obj = parser.parse(new FileReader("stockgen/dummy.json"));
-            JSONObject jsonObject = (JSONObject)obj;
+            JSONObject jsonObject = (JSONObject)parser.parse(new FileReader("stockgen/stock_transations-3.by.account.holder.json"));
+            // Object obj = parser.parse(new FileReader("stockgen/dummy.json"));
+            // JSONObject jsonObject = (JSONObject)obj;
+
             // JSONArray array = (JSONArray)obj;
             // System.out.println(array);
-            String Fname = (String)jsonObject.get("first_name");
-            String Lname = (String)jsonObject.get("last_name");
-            long accountNum = (long)jsonObject.get("account_number");
-            String social = (String)jsonObject.get("ssn");
-            String email = (String)jsonObject.get("email");
-            String phone = (String)jsonObject.get("phone");
-            String begBal = (String)jsonObject.get("beginning_balance");
 
-            System.out.println("Account number: " +accountNum);
-            System.out.println("Name: " + Fname + Lname);
-            System.out.println("Social: " + social);
-            System.out.println("Email: " + email);
-            System.out.println("Phone" + phone);
-            System.out.println("Begining bal: " + begBal);
+            
 
-            JSONArray stockTrades = (JSONArray)jsonObject.get("stock_trades");
-            Iterator iterator = stockTrades.iterator();
-            while(iterator.hasNext()){
-                System.out.println((iterator.next()));
-            }
+            // String Fname = (String)jsonObject.get("first_name");
+            // String Lname = (String)jsonObject.get("last_name");
+            // long accountNum = (long)jsonObject.get("account_number");
+            // String social = (String)jsonObject.get("ssn");
+            // String email = (String)jsonObject.get("email");
+            // String phone = (String)jsonObject.get("phone");
+            // String begBal = (String)jsonObject.get("beginning_balance");
+
+            // System.out.println("Account number: " +accountNum);
+            // System.out.println("Name: " + Fname + Lname);
+            // System.out.println("Social: " + social);
+            // System.out.println("Email: " + email);
+            // System.out.println("Phone" + phone);
+            // System.out.println("Begining bal: " + begBal);
+
+            // JSONArray stockTrades = (JSONArray)jsonObject.get("stock_trades");
+            // Iterator iterator = stockTrades.iterator();
+            // while(iterator.hasNext()){
+            //     System.out.println((iterator.next()));
+            // }
 
 
         }catch(Exception e){
