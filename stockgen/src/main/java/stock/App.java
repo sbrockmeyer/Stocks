bfc741b967c0345b5b9d86a5e4641ab9ca8a7a17
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-10-17 17:43:51 -0600

    im doing it!!

--- stockgen/src/main/java/stock/App.java
+++ stockgen/src/main/java/stock/App.java
@@ -17,12 +17,33 @@ public class App {
                 System.out.println(object.keySet());
 
                 System.out.println(object.get("account_number"));
+                System.out.println(object.get("first_name"));
+                System.out.println(object.get("last_name"));
+                System.out.println(object.get("ssn"));
+                System.out.println(object.get("email"));
+                System.out.println(object.get("phone"));
+
+                String beginningBal = (String)object.get("beginning_balance");
+                System.out.println(beginningBal);
 
                 JSONArray stockArray = (JSONArray) object.get("stock_trades");
 
                 for(int j = 0; j < stockArray.size(); j++){
-                    //if buy - remove cash
-                    //if sell - add
+                    JSONObject object2 = (JSONObject) stockArray.get(j);
+                    String type = (String) object2.get("type");
+                    System.out.println("________________________");
+                    System.out.println("a " + type);
+                    System.out.println(object2.get("type"));
+                    System.out.println(object2.get("stock_symbol"));
+                    System.out.println(object2.get("count_shares"));
+                    System.out.println(object2.get("price_per_share"));
+
+                    if (type == "buy") {
+                        System.out.println("Buyyyyyyyyyy");
+                    } else {
+                        System.out.println("Sellllllllll");
+                    }
+
                 }
             }
 
