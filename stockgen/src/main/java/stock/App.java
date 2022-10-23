012b2bcb49d6c9be223db8287b192e4a5bf5dc27
sbrockmeyer <79857788+sbrockmeyer@users.noreply.github.com>
2022-10-23 13:21:53 -0600

    6

--- stockgen/src/main/java/stock/App.java
+++ stockgen/src/main/java/stock/App.java
@@ -6,7 +6,7 @@ import org.json.simple.*;
 import org.json.simple.parser.*;
 
 public class App {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         Float newBal;
         Long accountNum;
@@ -20,95 +20,97 @@ public class App {
         Long countShare;
         String price;
         String symbol;
-        
-        try{
-            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("stockgen/stock_transations-3.by.account.holder.json"));
-            
-            for(int i = 0; i < 1; i++){
+        Float stockAmount;
+
+        try {
+            JSONArray jsonArray = (JSONArray) parser
+                    .parse(new FileReader("stockgen/stock_transations-3.by.account.holder.json"));
+
+            for (int i = 0; i < 1; i++) {
                 JSONObject object = (JSONObject) jsonArray.get(i);
                 System.out.println(object.keySet());
-                
+
                 accountNum = (Long) object.get("account_number");
                 fName = (String) object.get("first_name");
                 lName = (String) object.get("last_name");
                 social = (String) object.get("ssn");
                 email = (String) object.get("email");
                 phone = (String) object.get("phone");
-                
+
                 System.out.println(accountNum);
                 System.out.println(fName + " " + lName);
                 System.out.println(social);
                 System.out.println(email);
                 System.out.println(phone);
-                
-                
-                String beginningBal = (String)object.get("beginning_balance");
+
+                String beginningBal = (String) object.get("beginning_balance");
                 System.out.println(beginningBal);
-                
+
                 String begBal = beginningBal.replaceAll(("\\$"), "");
                 Float bal = Float.parseFloat(begBal);
-                
+
                 JSONArray stockArray = (JSONArray) object.get("stock_trades");
-                
-                for(int a = 0; a < stockArray.size(); a++){
+
+                for (int a = 0; a < stockArray.size(); a++) {
                     JSONObject object2 = (JSONObject) stockArray.get(a);
-                    
+
                     type = (String) object2.get("type");
-                    countShare = (Long) object2.get("count_shares"); 
+                    countShare = (Long) object2.get("count_shares");
                     Float shares = (Float) countShare.floatValue();
                     symbol = (String) object2.get("stock_symbol");
-                    Float startStock;
-                    
+                    Float startStock = 0f;
+
                     System.out.println("________________________");
                     // System.out.println("a " + type)/;
                     System.out.println(object2.get("type"));
                     System.out.println(symbol);
                     System.out.println(object2.get("count_shares"));
                     System.out.println(object2.get("price_per_share"));
-                    
+
                     price = (String) object2.get("price_per_share");
                     String priceShare = price.replaceAll(("\\$"), "");
                     Float sharePrice = Float.parseFloat(priceShare);
 
                     if (type.equals("Buy")) {
-                        Float fullCost = sharePrice*shares;
+                        Float fullCost = sharePrice * shares;
                         newBal = bal - fullCost;
-                        startStock =  +shares;
+                        stockAmount = startStock + shares;
                         System.out.println(startStock);
-                        System.out.println("here is new bal $"+newBal);
-                        
-                    }else if(type.equals("Sell")) {
-                        Float fullCost = sharePrice*shares;
-                        newBal = bal + fullCost;
-                        startStock= -shares;
-                        System.out.println(startStock);
-                        System.out.println( "here is new bal $"+ newBal);
+                        System.out.println("here is new bal $" + newBal);
 
-                    }else{
+                    } else if (type.equals("Sell")) {
+                        Float fullCost = sharePrice * shares;
+                        newBal = bal + fullCost;
+                        stockAmount = startStock - shares;
+                        System.out.println(startStock);
+                        System.out.println("here is new bal $" + newBal);
+
+                    } else {
                         System.out.println("something isnt right");
                     }
                 }
 
-                
-
                 File myObj = new File("stock.html");
-                if(myObj.createNewFile()){
+                if (myObj.createNewFile()) {
                     System.out.println("File created" + myObj.getName());
-                }else{
+                } else {
                     System.out.println("File already exists");
                 }
-    
+
                 FileWriter myWriter = new FileWriter("stock.html");
                 myWriter.write(
-                    "<p>" + accountNum + "</p>" +
-                    "<p>" + fName + " " + lName + "</p>" + 
-                    "<p>" + social + "</p>" 
-                    );
+                        "<p>" + "Full Name: " + fName + " " + lName + "</p>" +
+                        "<p>" + "Social Security Number: " + social + "</p>" +
+                        "<p>" + "Email: " + email + "</p>" + 
+                        "<p>" + "Phone: " + phone + "</p>" + 
+                        "<p>" + "Account Number:  " + accountNum + "</p>" + 
+                        "<p>"
+                        );
                 myWriter.close();
                 System.out.println("wrote to file");
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
